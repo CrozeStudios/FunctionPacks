d1fc98d8fedc18f7eeae533f0485cf5730b59113
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-07-09 12:11:02 +0300

    init

---
+++ src/OguzhanUmutlu/FunctionPacks/FunctionPacks.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace OguzhanUmutlu\FunctionPacks;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class FunctionPacks extends PluginBase {
+    public $functions = [];
+    public function onEnable() {
+        $dir = Server::getInstance()->getDataPath()."functions";
+        if(!file_exists($dir))
+            mkdir($dir);
+        foreach(scandir($dir) as $file) {
+            if($file != "." && $file != "..")
+                $this->functions[implode(".", array_slice(explode(".", $file), 0, count(explode(".", $file))-1))] = file_get_contents($dir."\\".$file);
+        }
+        $this->getLogger()->notice("Loaded ".count($this->functions)." functions: ".implode(", ", array_keys($this->functions)));
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if($command->getName() != "function" || !$sender->hasPermission($command->getPermission())) return false;
+        if(!isset($args[0])) {
+            $sender->sendMessage($command->getUsage());
+            return false;
+        }
+        if(!isset($this->functions[$args[0]])) {
+            $sender->sendMessage("Function not found!");
+            return false;
+        }
+        foreach(explode("\n", $this->functions[$args[0]]) as $function) {
+            $function = str_replace("%player", $sender->getName(), $function);
+            foreach(array_slice($args, 1) as $i => $arg)
+                $function = str_replace("%".($i-1), $arg, $function);
+            Server::getInstance()->dispatchCommand($sender, $function);
+        }
+        return true;
+    }
+}
